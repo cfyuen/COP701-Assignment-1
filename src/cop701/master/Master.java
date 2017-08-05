2cbc064fd7213c169d8a4a6d07d25f5d42af3d12
Chak Fai Yuen <mcs172525@iitd.ac.in>
2017-08-05 21:39:20 +0530

    Project skeleton

---
+++ src/cop701/master/Master.java
@@ -0,0 +1,14 @@
+package cop701.master;
+
+public class Master {
+
+	/**
+	 * This is the main entry point for the program
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/cop701/node/Client.java
@@ -0,0 +1,11 @@
+package cop701.node;
+
+public class Client {
+
+	/**
+	 * This is the main program for client node
+	 */
+	public Client() {
+		
+	}
+}
