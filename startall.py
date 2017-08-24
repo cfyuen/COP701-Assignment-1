6b3153a5606cf94823d14a9b08c38239bbf11433
Chak Fai Yuen <mcs172525@iitd.ac.in>
2017-08-24 16:03:58 +0530

    Mininet python script to allow scripts run in foreground

--- startall.py
+++ startall.py
@@ -1,4 +1,5 @@
 from mininet.cli import CLI
+from mininet.term import makeTerm
 import random
 import string
 import time
@@ -9,6 +10,11 @@ def startall( self, line ):
     for h in net.hosts:
         account = ''.join(random.choice('0123') for _ in range(8))
         print "Start java in " , h , " with account name " , account
-        h.sendCmd("java -jar ~/dev/A1.jar " , account)
+        cmdLine = "java -jar ~/dev/A1.jar " + account
+        print "  Calling command: " , cmdLine
+	# Running in foreground
+        net.terms += makeTerm(h, cmd=cmdLine)
+        # Running in background
+        # h.sendCmd(cmdLine)
         time.sleep(0.5)
 CLI.do_startall = startall
