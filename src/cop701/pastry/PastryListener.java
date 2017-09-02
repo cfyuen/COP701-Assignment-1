4b081fc816f42818e67772b4d7e7ff5250064b75
Chak Fai Yuen <mcs172525@iitd.ac.in>
2017-09-03 00:42:16 +0530

    Pastry leafSet bug fix

--- src/cop701/pastry/PastryListener.java
+++ src/cop701/pastry/PastryListener.java
@@ -15,41 +15,40 @@ public class PastryListener {
 		this.pastry = pastry;
 	}
 
-	public void doStuff(Object inObject) {
+	public void doStuff(Message m) {
 		
-		
-		if (inObject instanceof Message) {
-			Message m = (Message)inObject;
-			logger.info("Pastry message received");
-			System.out.println("**** " + m.getAddress() + " received from " + m.getSenderId() + " msg type of " + m.getMessageType());
-			if(m.getMessageType()==1)
-			{
-				this.pastry.sendNodesMap(m);
-			}
-			else if(m.getMessageType()==2)
-			{
-				this.pastry.addNodesMap(m);
-			}
-			else if(m.getMessageType()==4)
-			{
-				this.pastry.addBroadcastNodesMap(m);
-			}
-			else if(m.getMessageType()==5)
-			{
-				this.pastry.routeToNode(m);
-			}
-			else if(m.getMessageType()==6)
-			{
-				this.pastry.get(m.getSenderId(),m.getQueryAccountId());
-			}
-			else if(m.getMessageType()==7)
-			{
-				this.pastry.addDetails(m);
-			}
-			else if(m.getMessageType()==8)
-			{
-				this.pastry.sendLeftLeafSet(m);
-			}
+		logger.info("Pastry message received");
+		System.out.println("**** " + m.getAddress() + " received from " + m.getSenderId() + " msg type of " + m.getMessageType());
+		if(m.getMessageType()==1)
+		{
+			this.pastry.sendNodesMap(m);
+		}
+		else if(m.getMessageType()==2)
+		{
+			this.pastry.addNodesMap(m);
+		}
+		else if(m.getMessageType()==4)
+		{
+			this.pastry.addBroadcastNodesMap(m);
+		}
+		else if(m.getMessageType()==5)
+		{
+			this.pastry.routeToNode(m);
+		}
+		else if(m.getMessageType()==6)
+		{
+			this.pastry.get(m.getSenderId(),m.getQueryAccountId());
+		}
+		else if(m.getMessageType()==7)
+		{
+			this.pastry.addDetails(m);
+		}
+		else if(m.getMessageType()==8)
+		{
+			this.pastry.sendLeftLeafSet(m);
+		}
+		else {
+			logger.warning("Unknown message type");
 		}
 		
 	}
